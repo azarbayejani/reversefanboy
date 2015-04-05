29cb75a934f9ec630375fa2cca237725f4b0eb0e
Bobak Azarbayejani <bobby.azarbayejani@gmail.com>
2015-04-05 00:18:16 -0700

    Moved cloud-to-butt notice

--- src/content_script.js
+++ src/content_script.js
@@ -1,7 +1,4 @@
 import Walker from './walker';
 
-// I stole all this code from Cloud-to-butt.
-// https://github.com/panicsteve/cloud-to-butt
-
 var bodyWalker = new Walker(document.body)
 bodyWalker.walkRoot();
--- src/walker.js
+++ src/walker.js
@@ -1,5 +1,8 @@
 import Translator from './translator';
 
+// This is ES6-ified code I previously stole from cloud-to-butt:
+// https://github.com/panicsteve/cloud-to-butt
+
 export default class Walker {
 	constructor(root) {
 		this.root = root;
