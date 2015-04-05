cc936132cad4a9f3a212a4d745605890a1d607c1
Bobak Azarbayejani <bobby.azarbayejani@gmail.com>
2015-04-04 18:08:16 -0700

    Adds handling of S2/S3

--- source/content_script.js
+++ source/content_script.js
@@ -35,16 +35,18 @@ function handleText(textNode)
 		return $1 === "APPLE" ? "SAMSUNG" : "APPLE";
 	});
 	v = v.replace(//g,"Samsung ");
-	v = v.replace(/\b(((Galaxy S)(\d)?(\sI+)?)|(iPhone))\b/ig, function($1,$2,$3,$4,$5, $6) {
+	v = v.replace(/\b(((Galaxy S)((\d)|(\sI+))?)|(iPhone))\b/ig, function($1,$2,$3,$4,$5, $6, $7, $8) {
 		var retValue = "Galaxy S";
+
 		if ($4) {
 			retValue = ($4.toUpperCase() === "GALAXY S" ? "iPhone" : "iPhone ");
 		}
-		if ($6) {
-			console.log($6);
-			var countI = $6.split('').length - 1;
+		if($6) { retValue = retValue + " " + $6 }
+		if ($7) {
+			var countI = $7.split('').length - 1;
 			retValue = retValue + " " + countI;
 		}
+
 		return retValue;
 	});
 
